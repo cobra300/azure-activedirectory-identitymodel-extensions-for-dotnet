504c602605a0271686f15621d75a3a8cc58eec7b
Yan Chen <cyahe@microsoft.com>
2017-09-12 17:04:13 -0700

    Add CrossToken ValidateToken test cases

--- test/Microsoft.IdentityModel.Tests/IdentityUtilities.cs
+++ test/Microsoft.IdentityModel.Tests/IdentityUtilities.cs
@@ -25,12 +25,13 @@
 //
 //------------------------------------------------------------------------------
 
+using System;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Security.Cryptography;
 using System.Text;
-using System;
+using System.Xml;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.IdentityModel.Tokens.Saml2;
 
@@ -91,6 +92,100 @@ namespace Microsoft.IdentityModel.Tests
             JwtPayload payload = new JwtPayload(issuer, audience, claims, nbf, exp, iat);
             JwtHeader header = (signingCredentials != null) ? new JwtHeader(signingCredentials) : new JwtHeader();
             return new JwtSecurityToken(header, payload, header.Base64UrlEncode(), payload.Base64UrlEncode(), "" );
+        }
+
+        public static string CreateSamlSecurityToken(SecurityTokenDescriptor securityTokenDescriptor, SecurityTokenHandler tokenHandler)
+        {
+            SecurityToken securityToken = tokenHandler.CreateToken(securityTokenDescriptor);
+            StringBuilder sb = new StringBuilder();
+            XmlWriter writer = XmlWriter.Create(sb);
+            tokenHandler.WriteToken(writer, securityToken);
+            writer.Flush();
+            writer.Close();
+            return sb.ToString();
+        }
+
+        public static string CreateSaml2SecurityToken(SecurityTokenDescriptor securityTokenDescriptor, SecurityTokenHandler tokenHandler)
+        {
+            SecurityToken securityToken = tokenHandler.CreateToken(securityTokenDescriptor);
+            StringBuilder sb = new StringBuilder();
+            XmlWriter writer = XmlWriter.Create(sb);
+            tokenHandler.WriteToken(writer, securityToken);
+            writer.Flush();
+            writer.Close();
+            return sb.ToString();
+        }
+
+        public static string CreateJwtSecurityToken(SecurityTokenDescriptor securityTokenDescriptor, SecurityTokenHandler tokenHandler)
+        {
+            SecurityToken securityToken = tokenHandler.CreateToken(securityTokenDescriptor);
+            return tokenHandler.WriteToken(securityToken);
+        }
+
+        public static SecurityTokenDescriptor DefaultX509SecurityTokenDescriptor
+        {
+            get { return DefaultSecurityTokenDescriptor(DefaultX509SigningCredentials); }
+        }
+
+        public static SecurityTokenDescriptor DefaultSecurityTokenDescriptor(SigningCredentials signingCredentials)
+        {
+            return new SecurityTokenDescriptor
+            {
+                Audience = DefaultAudience,
+                SigningCredentials = signingCredentials,
+                Subject = DefaultClaimsIdentity,
+                Issuer = DefaultIssuer,
+                NotBefore = DateTime.UtcNow,
+                Expires = DateTime.UtcNow + TimeSpan.FromDays(1)
+            };
+        }
+
+        public static TokenValidationParameters DefaultX509TokenValidationParameters
+        {
+            get { return DefaultTokenValidationParameters(DefaultX509SigningKey); }
+        }
+
+        public static TokenValidationParameters DefaultTokenValidationParameters(SecurityKey securityKey)
+        {
+            return new TokenValidationParameters
+            {
+                AuthenticationType = DefaultAuthenticationType,
+              //  IssuerSigningToken = securityToken,
+                IssuerSigningKey = securityKey,
+                ValidAudience = DefaultAudience,
+                ValidIssuer = DefaultIssuer,
+            };
+        }
+
+        public static string DefaultAudience { get { return "http://relyingparty.com"; } }
+        public static SecurityKey DefaultX509SigningKey { get { return KeyingMaterial.DefaultX509Key_2048; ; } }
+
+        public static ClaimsIdentity DefaultClaimsIdentity
+        {
+            get
+            {
+                return new ClaimsIdentity(DefaultClaims, DefaultAuthenticationType);
+            }
+        }
+
+        public static SigningCredentials DefaultX509SigningCredentials { get { return KeyingMaterial.DefaultX509SigningCreds_2048_RsaSha2_Sha2; } }
+        public const string DefaultAuthenticationType = "Federation";
+        public const string DefaultIssuer = "http://gotjwt.com";
+        public const string DefaultOriginalIssuer = "http://gotjwt.com/Original";
+
+        public static IEnumerable<Claim> DefaultClaims
+        {
+            get
+            {
+                return new List<Claim>()
+                {
+                    new Claim(ClaimTypes.Country, "USA", ClaimValueTypes.String, DefaultIssuer, DefaultIssuer),
+                    new Claim(ClaimTypes.Email, "user@contoso.com", ClaimValueTypes.String, DefaultIssuer, DefaultIssuer),
+                    new Claim(ClaimTypes.GivenName, "Tony", ClaimValueTypes.String, DefaultIssuer, DefaultIssuer),
+                    new Claim(ClaimTypes.HomePhone, "555.1212", ClaimValueTypes.String, DefaultIssuer, DefaultIssuer),
+                    new Claim(ClaimTypes.Role, "Sales", ClaimValueTypes.String, DefaultIssuer, DefaultIssuer),
+                };
+            }
         }
 
 #if NET452
